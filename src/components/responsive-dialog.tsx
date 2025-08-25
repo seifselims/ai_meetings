9b676830c20f3cbc6668bcd33c7c4143369c6273
seifselims <124066228+seifselims@users.noreply.github.com>
2025-08-25 17:14:24 +0300

    Responsive Dialog

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,64 @@
+'use client'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog'
+
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+} from '@/components/ui/drawer'
+import { useIsMobile } from '@/hooks/use-mobile'
+
+
+interface ResponsiveDialogProps {
+    title: string
+    description: string
+    children: React.ReactNode
+    open: boolean
+    onOpenChange: (open: boolean) => void
+}
+
+export const ResponsiveDialog = ({ title, description, children, open, onOpenChange }: ResponsiveDialogProps) => {
+    const isMobile = useIsMobile()
+    if(isMobile) {
+        return (
+            <Drawer open={open} onOpenChange={onOpenChange}>
+                <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>
+                        {title}
+                    </DrawerTitle>
+                    <DrawerDescription>
+                        {description}
+                    </DrawerDescription>
+                    </DrawerHeader>
+                        {children}
+                </DrawerContent>
+            </Drawer>
+        )
+    }
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        {title}
+                    </DialogTitle>
+                    <DialogDescription>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+                    <div className='p-4'>
+                        {children}
+                    </div>
+                </DialogContent>
+        </Dialog>
+    )
+}
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,6 +2,8 @@
 
 import { ErrorState } from "@/components/error-state"
 import { LoadingState } from "@/components/loading-state"
+import { ResponsiveDialog } from "@/components/responsive-dialog"
+import { Button } from "@/components/ui/button"
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query"
 import React, { useState } from 'react'
