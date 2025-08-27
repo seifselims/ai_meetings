56098093f0d9f7f519af097a57e1593751902e61
seifselims <124066228+seifselims@users.noreply.github.com>
2025-08-27 22:48:01 +0300

    Update and Delete Agent

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -44,6 +44,21 @@ export const AgentForm = ({onSuccess, onCancel, initialValues}:AgentFormProps) =
         },
     }),
    )
+   const updateAgent = useMutation(
+    trpc.agents.update.mutationOptions({
+        onSuccess: async () => {
+            await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}))
+            if(initialValues?.id) {
+                await queryClient.invalidateQueries(trpc.agents.getOne.queryOptions({id: initialValues.id}))
+            }
+            onSuccess?.()
+        },
+        onError:(error) => {
+            toast.error(error.message)
+            // TODO: check if error code is "FORBIDDEN", redirect to "/upgrade"
+        },
+    }),
+   )
    const form = useForm<z.infer<typeof agentsInsertSchema>>({
     resolver:zodResolver(agentsInsertSchema),
     defaultValues: {
@@ -52,10 +67,10 @@ export const AgentForm = ({onSuccess, onCancel, initialValues}:AgentFormProps) =
     }
    })
    const  isEdit = !!initialValues?.id
-   const isPending = createAgent.isPending
+   const isPending = createAgent.isPending || updateAgent.isPending
    const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
     if(isEdit) {
-        console.log("TODO: updateAgent")
+        updateAgent.mutate({...values, id: initialValues?.id})
     }
     else{
         createAgent.mutate(values)
--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -1,26 +1,63 @@
 'use client'
 
 import { useTRPC } from "@/trpc/client"
-import { useSuspenseQuery } from "@tanstack/react-query"
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query"
 import { AgentIdViewHeader } from "../components/agent-id-view-header"
 import { GeneratedAvatar } from "@/components/generated-avatar"
 import { Badge } from "@/components/ui/badge"
 import { VideoIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+import { useConfirm } from "@/hooks/use-confirm"
+import { useState } from "react"
+import { UpdateAgentDialog } from "../components/update-agent-dialog"
 
 interface Props {
     agentId: string
 }
 
 export const AgentIdView = ({agentId}: Props) => {
+    const router = useRouter()
     const trpc = useTRPC()
+    const queryClient = useQueryClient()
+    const [updateAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false)
     const {data} = useSuspenseQuery(trpc.agents.getOne.queryOptions({id:agentId}))
+    const removeAgent = useMutation(
+        trpc.agents.remove.mutationOptions({
+            onSuccess:() => {
+                queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}));
+                queryClient.invalidateQueries(trpc.agents.getOne.queryOptions({id:agentId}));
+                // TODO: invalidate free tier usage
+                router.push("/agents")
+            },
+            onError: (error) => {
+                toast.error(error.message)
+            }
+        }),
+    )
+    const [RemoveConfirmation, confirmRemove] =useConfirm(
+        "Are you sure?",
+        `The following agent will be permanently deleted: ${data.name} along with all of its ${data.meetingCount} meetings. This action cannot be undone.`,
+    )
+    const handleRemoveAgent = async () => {
+        const ok = await confirmRemove()
+        if(!ok) return
+        await removeAgent.mutateAsync({id: agentId})
+    }
 return (
+    <>
+    <RemoveConfirmation/>
+    <UpdateAgentDialog
+        open={updateAgentDialogOpen}
+        onOpenChange={setUpdateAgentDialogOpen}
+        initialValues={data}
+    />
     <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
     <AgentIdViewHeader
         agentId={agentId}
         agentName={data.name}
-        onEdit={()=> {}}
-        onRemove={()=> {}}
+        onEdit={()=> setUpdateAgentDialogOpen(true)}
+        onRemove={handleRemoveAgent}
 
     />
     <div className="bg-white rounded-lg border">
@@ -48,5 +85,6 @@ return (
 
     </div>
     </div>
+    </>
 )
 }
